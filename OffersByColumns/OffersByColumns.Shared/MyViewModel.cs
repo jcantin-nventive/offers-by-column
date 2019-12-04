fa45192a3a801e23370f1f870a104ecb7c2f625c
Julie Cantin <julie.cantin@nventive.com>
2019-12-04 10:32:45 -0500

    display list of lists with ListView

---
+++ OffersByColumns/OffersByColumns.Shared/MyViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OffersByColumns
+{
+	public class MyViewModel
+	{
+		public IEnumerable<IEnumerable<string>> CategoriesWithOffers => Enumerable.Range(0, 5).Select(idx => Enumerable.Range(idx * 5, 5).Select(nb => $"Item #{nb}"));
+	}
+}
